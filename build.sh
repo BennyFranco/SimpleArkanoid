bc627d069b6dd143f91d8210db321f6972ea8c8a
Benny Franco Dennis <bennyfranco@icloud.com>
2018-06-14 20:15:04 -0700

    Simple build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!bin/bash
-clang++ SimpleArkanoid.cpp -o SimpleArkanoid -std=c++11 -stdlib=libc++ -mmacosx-version-min=10.11 \
+clang++ SimpleArkanoid.cpp -o build/SimpleArkanoid -std=c++11 -stdlib=libc++ -mmacosx-version-min=10.11 \
          -Wl,-rpath,. -L./lib/ -lsfml-window -lsfml-graphics -lsfml-system
-mv SimpleArkanoid ./build/SimpleArkanoid
 cd lib
 cp -r ./ ../build
 cd ..
-#ld -o Arkanoid SimpleArkanoid.o ./lib/libsfml-window.2.5.0.dylib ./lib/libsfml-graphics.2.5.0.dylib ./lib/libsfml-system.2.5.0.dylib ./lib/libsfml-audio.2.5.0.dylib ./lib/libsfml-network.2.5.0.dylib
