15b8ebb6b88bc17cdf43a47f84044fb0d8446470
Benny Franco Dennis <bennyfranco@icloud.com>
2018-06-14 21:19:33 -0700

    Ball struct added, building and debugging tasks added

--- SimpleArkanoid.cpp
+++ SimpleArkanoid.cpp
@@ -5,9 +5,30 @@ using namespace std;
 using namespace sf;
 
 constexpr int windowWidth{800}, windowHeight{600};
+constexpr float ballRadius{10.0f};
+
+struct Ball
+{
+    // SFML class that defines a circle shape
+    CircleShape shape;
+
+    // Ball constructor
+    // mX is the x coordinate
+    // mY is the y coordinate
+    Ball(float mX, float mY)
+    {
+        shape.setPosition(mX, mY);
+        shape.setRadius(ballRadius);
+        shape.setFillColor(Color::Red);
+        shape.setOrigin(ballRadius, ballRadius);
+    }
+};
 
 int main()
 {
+    // Ball instance
+    Ball ball{windowWidth / 2, windowHeight / 2};
+
     RenderWindow window{{windowWidth, windowHeight}, "Simple Arkanoid"};
     window.setFramerateLimit(60);
 
--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!bin/bash
+echo "Building and linking SimpleArkanoid"
 clang++ SimpleArkanoid.cpp -o build/SimpleArkanoid -std=c++11 -stdlib=libc++ -mmacosx-version-min=10.11 \
          -Wl,-rpath,. -L./lib/ -lsfml-window -lsfml-graphics -lsfml-system
 cd lib
+echo "Copying libs"
 cp -r ./ ../build
 cd ..
+echo "Ready"
